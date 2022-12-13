7642265e619912d9d61dc9b328a84591854767b5
Sebastian Wesołowski <Sebastian.wesolowski.sw@gmail.com>
2022-12-13 10:14:16 +0100

    start point - chapter 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Header title="Hello World">
+                    <div>Some content</div>
+                </Header>
+            </header>
+        </div>
+    );
 }
 
 export default App;
